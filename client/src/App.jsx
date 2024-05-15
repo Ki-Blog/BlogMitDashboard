715007998b60031e8dceb2d9f240576e9637cbf2
onben <ons.benyoussef12@outlook.com>
2024-05-15 21:28:12 +0200

    add  Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
       <Route path="/signup" element={<SignUp />} />
 
       </Routes>
+      <Footer />
     </BrowserRouter>
     
   )
