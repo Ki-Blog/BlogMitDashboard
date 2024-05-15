313cc76585de347770ffadfdea4bc418009393c7
Mone19 <fuocoangelos@yahoo.de>
2024-05-15 19:14:36 +0200

    create Home.jsx and first Route frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <h1>App</h1>
+    <Routes>
+
+    <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+
+
+<div>
+<h1>Dein Guide für die AI-Revolution.</h1>
+<p>Tauche ein in die faszinierende Welt der AI - Schritt für Schritt!</p>
+</div>
