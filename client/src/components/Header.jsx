802a9423613307d580223f05f5c2ef23813aaaa0
Mone19 <fuocoangelos@yahoo.de>
2024-05-22 09:51:20 +0200

    Header Logo finalisiert, responsive, Navigation Animation optimiert

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,24 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Button, Dropdown } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { BsFillSunFill, BsMoonStarsFill } from "react-icons/bs";
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
-import { useEffect, useState } from 'react';
-import logo from "../images/logo.png"
-import logodark from "../images/logodark.png"
-import { ImSun } from "react-icons/im";
-import "../index.css"
+import logo from "../images/logo.png";
+import logodark from "../images/logodark.png";
+import "../index.css";
 
 export default function Header() {
-  const path = useLocation().pathname;
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
   const linkClass = theme === 'dark' ? 'navbar-link-dark' : 'navbar-link';
+  const linkClassDesktop = theme === 'dark' ? 'navbar-link-dark-desktop' : 'navbar-link-desktop';
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -39,6 +38,7 @@ export default function Header() {
         console.log(data.message);
       } else {
         dispatch(signoutSuccess());
+        navigate('/');
       }
     } catch (error) {
       console.log(error.message);
@@ -53,86 +53,126 @@ export default function Header() {
     navigate(`/search?${searchQuery}`);
   };
 
+  const getLinkClass = (path) => {
+    return location.pathname === path ? `link-active ${linkClassDesktop}` : linkClassDesktop;
+  };
+
   return (
-    <Navbar className='border-b border-2px border-[#0e93b7]  dark:bg-[#0c12243f]'>
-      <Link to='/'>
-        <img src={theme === "dark" ? logodark : logo} alt="Logo" />
-      </Link>
-      <form onSubmit={handleSubmit}>
-        <TextInput
+    <nav className="bg-[#b8bfd71e] border-[#385cb6] dark:bg-[#0b1020d4] border-b-2">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link to="/" className="flex items-center">
+            <img src={theme === "dark" ? logodark : logo} alt="Logo" />
+          </Link>
+        </div>
+        {/* Desktop Navigation */}
+        <div className="hidden md:flex flex-grow justify-center">
+        <form onSubmit={handleSubmit}>
+        <input 
           type='text'
           placeholder='Search...'
-          rightIcon={AiOutlineSearch}
-          className='hidden lg:inline bg-white dark:bg-black text-left rounded-md'
+          className='hidden lg:inline p-2 border dark:bg-[#0b1020d4] bg-[#b8bfd71e] rounded-md mr-10'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <AiOutlineSearch />
-      </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button
-          className='w-12 h-10 hidden sm:inline'
-          color='gray'
-          pill
-          onClick={() => dispatch(toggleTheme())}
-        >
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
-        </Button>
-
-        {currentUser ? (
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar alt='user' img={currentUser.profilePicture} rounded />
-            }
-          >
-            <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.username}</span>
-              <span className='block text-sm font-medium truncate'>
-                {currentUser.email}
-              </span>
-            </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
-              <Dropdown.Item>Profile</Dropdown.Item>
-            </Link>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-          </Dropdown>
-        ) : (
-          <Link to='/signin'>
-            <Button className='bg-#0e93b7' outline>
-              Sign In
-            </Button>
-          </Link>
-        )}
-        
-      <Navbar.Toggle />
+      <button className=''>
+      </button>
+          <ul className="font-medium flex space-x-8 mt-[8px]">
+            <li>
+              <Link
+                to="/"
+                className={`py-1 px-3 hover:text-[#385cb6] ${linkClass} ${getLinkClass('/')}`}
+                aria-current={location.pathname === '/' ? 'page' : undefined}
+              >
+                Home
+              </Link>
+            </li>
+            <div className="hidden lg:inline" style={{ borderLeft: '1px solid grey', height: '25px', paddingRight: '8px' }}></div>
+            <li>
+              <Link
+                to="/about"
+                className={`py-1 px-3 hover:text-[#385cb6] ${linkClass} ${getLinkClass('/about')}`}
+                aria-current={location.pathname === '/about' ? 'page' : undefined}
+              >
+                Über uns
+              </Link>
+            </li>
+            <div className="hidden lg:inline" style={{ borderLeft: '1px solid grey', height: '25px', paddingRight: '8px' }}></div>
+            <li>
+              <Link
+                to="/search"
+                className={`py-1 px-3 hover:text-[#385cb6] ${linkClass} ${getLinkClass('/search')}`}
+                aria-current={location.pathname === '/search' ? 'page' : undefined}
+              >
+                Alle Beiträge
+              </Link>
+            </li>
+          </ul>
         </div>
-        <Navbar.Collapse >
-          <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to="/" className={linkClass} >Startseite</Link>
-          </Navbar.Link>
 
-          {window.innerWidth > 500 &&
-            <div style={{ borderLeft: '1px solid grey', height: '25px', paddingRight: '8px' }}></div>
-          }
+        <div className="flex items-center space-x-4">
+          {currentUser ? (
+            <Dropdown className='dark:bg-[#2b3456]'
+              arrowIcon={false}
+              inline
+              label={<Avatar alt='user' img={currentUser.profilePicture} rounded />}
+            >
+              <Dropdown.Header>
+                <span className='block text-sm'>@{currentUser.username}</span>
+                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+              </Dropdown.Header>
+              <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to='/signin'>
+              <Button gradientDuoTone='purpleToBlue' outline>
+                Sign In
+              </Button>
+            </Link>
+          )}
+          <Button onClick={() => dispatch(toggleTheme())} className="gap-2" pill color={"blue"}>
+            {theme === 'dark' ? <BsFillSunFill /> : <BsMoonStarsFill />}
+          </Button>
+        </div>
 
-          <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to="/about" className={linkClass} >Über uns</Link>
-          </Navbar.Link>
+        {/* Mobile Navigation */}
+        <button 
+          data-collapse-toggle="navbar-default" 
+          type="button" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-800 dark:focus:ring-gray-600" 
+          aria-controls="navbar-default" 
+          aria-expanded={isNavbarOpen} 
+          onClick={() => setIsNavbarOpen(!isNavbarOpen)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+          </svg>
+        </button>
 
-          {window.innerWidth > 500 &&
-            <div style={{ borderLeft: '1px solid grey', height: '25px', paddingRight: '8px' }}></div>
-          }
-
-          <Navbar.Link active={path === "/search"} as={"div"}>
-            <Link to="/search" className={linkClass}>Alle Beiträge</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
-    </Navbar>
+        <div className={`w-full md:hidden ${isNavbarOpen ? 'block' : 'hidden'}`} id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 dark:  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
+            <li>
+              <Link to="/" className={`py-1 px-3 text-gray-900 hover:text-[#385cb6] active:text-[#385cb6] md:p-0 ${linkClass}`}>Home</Link>
+            </li>
+            <hr className="my-4 border-gray-300 md:hidden"/>
+            <li>
+              <Link to="/about" className={`py-1 px-3 text-gray-900 hover:text-[#385cb6] active:text-[#385cb6] md:p-0 ${linkClass}`}>Über uns</Link>
+            </li>
+            <hr className="my-4 border-gray-300 md:hidden"/>
+            <li>
+              <Link to="/search" className={`py-1 px-3 text-gray-900 hover:text-[#385cb6] active:text-[#385cb6] md:p-0 ${linkClass}`}>Alle Beiträge</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
+
+
