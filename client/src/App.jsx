574239cb034d45d6603cd618be4e24e4c767c051
onben <ons.benyoussef12@outlook.com>
2024-05-22 14:59:36 +0200

    add  posts in frontend but not finished  and solve some errors between backend and frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,15 @@ import About from "./pages/About"
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Search from "./pages/Search"
+import PrivateRoute from "./components/PrivateRoute"
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
+import CreatePost from "./pages/CreatePost"
+import PostPage from "./pages/PostPage"
 import ScrollToTop from "./components/ScrollToTop"
+import Search from "./pages/Search"
 
 export default function App() {
   return (
@@ -19,6 +24,11 @@ export default function App() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/search" element={<Search />} />
+      <Route element={<PrivateRoute />}></Route>
+      <Route element={<OnlyAdminPrivateRoute />}>
+      <Route path="/create-post" element={<CreatePost/>} />
+      </Route> 
+      <Route path="/post/:postSlug" element={<PostPage/>} />
       </Routes>
 
     <Footer />
