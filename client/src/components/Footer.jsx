d679f44568f14cadaee3e0139f4d6e5c3e8e2ad8
Mone19 <fuocoangelos@yahoo.de>
2024-05-27 18:22:32 +0200

    style: Anpassungen der Verlinkung und Anordnung jetzt 1 zeilig

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,66 +1,34 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import {BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble} from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
 import logo from "../images/logo_gross.svg";
 import { useSelector } from "react-redux";
+
 export default function FooterCom() {
   const { theme } = useSelector((state) => state.theme);
+
   return (
     <Footer container className='border-t-2 border-[#9bb0ddd3] rounded-none dark:bg-[#090d1c] bg-[#f7f7fa]'>
-    <div className='w-full max-w-7xl mx-auto'>
-      <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
-        <div className='mt-5'>
-        <Link to="/">
-        <img src={logo} alt="Logo" className={`h-11 ${theme === 'dark' ? 'filter invert' : ''}`} />
-      </Link>
-      </div >
-      <div className='flex gap-16 sm:mt-0 mt-4 sm:justify-center'>
-        <div >
-        <Footer.Title title='Über Uns'/>
-        <Footer.LinkGroup col>
-          <Footer.Link
-            href='#'
-            rel='noopener noreferrer'
-          >
-            Github
-          </Footer.Link>
-          <Footer.Link
-            href='https://www.github.com/'
-            rel='noopener noreferrer'
-          >
-            AI Quantum 
-          </Footer.Link>
-          </Footer.LinkGroup>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='flex justify-between items-center py-5'>
+          <Link to="/" className='flex items-center'>
+            <img src={logo} alt="Logo" className={`h-11 ${theme === 'dark' ? 'filter invert' : ''}`} />
+          </Link>
+          <div className='flex items-center gap-4'>
+            <span className='text-lg font-semibold'>Lerne uns kennen:</span>
+            <Link to='/about' className='text-lg text-blue-500 hover:underline'>
+              Über uns
+            </Link>
+            <a href='https://github.com/Ki-Blog/BlogMitDashboard/' target='_blank' rel='noopener noreferrer'>
+              <BsGithub className='text-2xl' />
+            </a>
+          </div>
         </div>
-        <div>
-        <Footer.Title title='Legal'/>
-        <Footer.LinkGroup col>
-          <Footer.Link
-            href='/#'
-          >
-            Datenschutzrichtlinie
-          </Footer.Link>
-          <Footer.Link
-            href='/#'>
-            Bedingungen
-          </Footer.Link>
-          </Footer.LinkGroup>
+        <Footer.Divider />
+        <div className='w-full flex justify-between items-center py-4'>
+          <Footer.Copyright href='#' by="AI Quantum " year={new Date().getFullYear()} />
         </div>
       </div>
-    </div>
-    <Footer.Divider />
-    <div className='w-full sm:flex sm:items-center sm:justify-between'>
-      <Footer.Copyright href='#' by="AI Quantum " year={new Date().getFullYear()}
-      />
-      <div className='flex gap-8 sm:mt-0 mt-4 mr-16 sm:justify-center'>
-        <Footer.Icon href='#' icon={BsFacebook}/>
-        <Footer.Icon href='#' icon={BsInstagram}/>
-        <Footer.Icon href='#' icon={BsTwitter}/>
-        <Footer.Icon href='https://www.github.com/' icon={BsGithub}/>
-        <Footer.Icon href='#' icon={BsDribbble}/>
-      </div>
-    </div>
-  </div>
     </Footer>
   );
 }
