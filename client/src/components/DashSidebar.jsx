e8ec38188bd23c0d4a682285ff95b381284cdd11
Mone19 <fuocoangelos@yahoo.de>
2024-05-27 18:19:23 +0200

    style: Texte geändert zu Dein Profil, Deine Beiträge, Alle Benutzer, Alle Kommentare

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -9,6 +9,8 @@ import {
   HiUserGroup,
   HiCog,
   HiChartBar,
+  HiUser,
+  HiShieldCheck,
 } from 'react-icons/hi';
 
 export default function DashSidebar() {
@@ -44,6 +46,14 @@ export default function DashSidebar() {
   return (
     <div className="w-full md:w-56 h-full md:border-r border-[#aeaeae77] dark:border-[#aeaeae2f] dark:bg-[#090d1c] bg-[#f7f7fa] flex flex-col p-4">
       <div className="flex flex-col gap-1">
+        <div className="flex items-center p-3">
+          {currentUser && currentUser.isAdmin ? (
+            <HiShieldCheck className="mr-2 text-3xl text-[#8a52f3dd]" />
+          ) : (
+            <HiUser className="mr-2 text-3xl text-[#2ca3c1]" />
+          )}
+          <span>{currentUser.isAdmin ? 'Admin' : 'User'}</span>
+        </div>
         {currentUser && currentUser.isAdmin && (
           <Link to="/dashboard?tab=dash">
             <div className={`flex items-center p-3 rounded text-[#979dc0] hover:text-white hover:bg-[#141c37] ${tab === 'dash' || !tab ? 'bg-[#2e406d] text-white' : ''}`}>
@@ -55,14 +65,14 @@ export default function DashSidebar() {
         <Link to="/dashboard?tab=profile">
           <div className={`flex items-center p-3 rounded text-[#979dc0] hover:text-white hover:bg-[#141c37] ${tab === 'profile' ? 'bg-[#2e406d] text-white' : ''}`}>
             <HiCog className="mr-2 text-2xl" />
-            Profil
+            Dein Profil
           </div>
         </Link>
         {currentUser.isAdmin && (
           <Link to="/dashboard?tab=posts">
             <div className={`flex items-center p-3 rounded text-[#979dc0] hover:text-white hover:bg-[#141c37] ${tab === 'posts' ? 'bg-[#2e406d] text-white' : ''}`}>
               <HiClipboardList className="mr-2 text-2xl" />
-              Beiträge
+              Deine Beiträge
             </div>
           </Link>
         )}
@@ -71,13 +81,13 @@ export default function DashSidebar() {
             <Link to="/dashboard?tab=users">
               <div className={`flex items-center p-3 rounded text-[#979dc0] hover:text-white hover:bg-[#141c37] ${tab === 'users' ? 'bg-[#2e406d] text-white' : ''}`}>
                 <HiUserGroup className="mr-2 text-2xl" />
-                Benutzer
+                Alle Benutzer
               </div>
             </Link>
             <Link to="/dashboard?tab=comments">
               <div className={`flex items-center p-3 rounded text-[#979dc0] hover:text-white hover:bg-[#141c37] ${tab === 'comments' ? 'bg-[#2e406d] text-white' : ''}`}>
                 <HiChatAlt2 className="mr-2 text-2xl" />
-                Kommentare
+                Alle Kommentare
               </div>
             </Link>
           </>
