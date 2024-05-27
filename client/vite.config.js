9a3c74062bb24a93182d8ce7b2e6712f91466e88
Sadek-Murad <sadekmurad@outlook.de>
2024-05-27 15:53:43 +0200

    feat: mircoserivce has been added

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api':{
+      '/api/auth':{
         target: "http://localhost:4000",
+        secure: false,
+      },
+      '/api/user':{
+        target: "http://localhost:4001",
         secure: false,
       },
     },
