efe14e49ab32f2dab0531535e47e32e30bbb243a
Sadek-Murad <sadekmurad@outlook.de>
2024-05-28 12:33:47 +0200

    fix: routes has modifed

--- api/comment/controllers/comment.controller.js
+++ api/comment/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import Comment from "../models/comment.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createComment = async (req, res, next) => {
   try {
@@ -20,10 +21,10 @@ export const createComment = async (req, res, next) => {
     next(error);
   }
 };
+
 export const getPostComments = async (req, res, next) => {
   try {
-    const comments = await Comment.find({ postId: req.params.postId }).sort({ createdAt: -1,
-    });
+    const comments = await Comment.find({ postId: req.params.postId }).sort({ createdAt: -1 });
     res.status(200).json(comments);
   } catch (error) {
     next(error);
@@ -52,26 +53,26 @@ export const likeComment = async (req, res, next) => {
 };
 
 export const editComment = async (req, res, next) => {
-try {
-  const comment = await Comment.findById(req.params.commentId);
-  if (!comment) {
-    return next(errorHandler(404, 'Kommentar wurde nicht gefunden'));
-  }
-  if (comment.userId !== req.user.id && !req.user.isAdmin) {
-    return next(errorHandler(403, 'Du bist nicht berechtigt, diesen Kommentar zu bearbeiten'));
-  }
-  const editedComment = await Comment.findByIdAndUpdate(
-    req.params.commentId,
-    {
-      content: req.body.content,
-    },
-    { new: true }
-  );
-  res.status(200).json(editedComment);
-  }   catch (error) {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, 'Kommentar wurde nicht gefunden'));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(errorHandler(403, 'Du bist nicht berechtigt, diesen Kommentar zu bearbeiten'));
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    res.status(200).json(editedComment);
+  } catch (error) {
     next(error);
   }
-}
+};
 
 export const deleteComment = async (req, res, next) => {
   try {
@@ -87,7 +88,7 @@ export const deleteComment = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const getcomments = async (req, res, next) => {
   if (!req.user.isAdmin)
@@ -99,13 +100,14 @@ export const getcomments = async (req, res, next) => {
     const comments = await Comment.find({})
       .sort({ createdAt: sortDirection })
       .skip(startIndex)
-      .limit(limit);
-      const totalComments = await Comment.countDocuments();
-      const now = new Date();
-      const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
-      const lastMonthComments = await Comment.countDocuments({ createdAt: { $gte: oneMonthAgo } });
-      res.status(200).json({ comments, totalComments, lastMonthComments });
+      .limit(limit)
+      .populate('postId');
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+    const lastMonthComments = await Comment.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
   } catch (error) {
     next(error);
   }
-}
+};
