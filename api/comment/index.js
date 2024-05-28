fe44508aa9bc48963cc56d2e1922be2d5494a475
Mikhail Sarnov <kpblm2@gmail.com>
2024-05-28 14:10:30 +0200

    fix: corrected empty comment.route.js and index.js in comment

--- api/comment/index.js
+++ api/comment/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import authRoutes from './routes/comment.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 
 
@@ -28,7 +28,7 @@ app.listen(4003, () => {
 });
 
 
-app.use("/api/comment", authRoutes);
+app.use("/api/comment", commentRoutes);
 
 
 
