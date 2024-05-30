a359e061902915cff630e9baa436fae6915cb956
Mikhail Sarnov <kpblm2@gmail.com>
2024-05-30 19:43:10 +0200

    feat: tf corrected

--- infra/main.tf
+++ infra/main.tf
@@ -50,5 +50,5 @@ resource "aws_s3_bucket_website_configuration" "web-config" {
 
 output "website_url" {
   description = "My website URL"
-  value = aws_s3_bucket_website_configuration.web-config.website_endpoint
+  value       = aws_s3_bucket_website_configuration.web-config.website_endpoint
 }
