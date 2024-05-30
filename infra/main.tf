4c70c0a4db632e0f8822fd9f0307c53187783367
Mikhail Sarnov <kpblm2@gmail.com>
2024-05-30 16:57:54 +0200

    feat: corrected tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,13 +3,17 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "react_app_bucket" {
-  bucket = "aiquantum-bucket" # This is the actual name of the S3 bucket on AWS
-  acl    = "public-read"
+  bucket = "aiquantum-bucket"
+  force_destroy = true
 
   tags = {
     Name        = "ReactAppBucket"
     Environment = "Production"
   }
+}
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.react_app_bucket.bucket
+  acl    = "private"
 }
 
 output "bucket_url" {
