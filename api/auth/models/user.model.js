a3765ef71e4dea55a0e7a2d8efcbe22df4c23cfd
Mone19 <fuocoangelos@yahoo.de>
2024-05-31 10:19:38 +0200

    fix: new link to Profilepicture

--- api/auth/models/user.model.js
+++ api/auth/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   profilePicture: {
     type: String,
-    default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    default: "http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/assets/profil.jpg",
   },
   isAdmin: {
     type: Boolean,
