bd6ac6c34eb9c6eb7432ec4ad58b516bea7f7adf
Mikhail Sarnov <kpblm2@gmail.com>
2024-06-19 12:12:17 +0200

    fix: firebase corrected

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -34,7 +34,7 @@ export default function SignIn() {
     }
     try {
       dispatch(signInStart());
-      const res = await fetch(`${baseUrl}api/auth/signin`, {
+      const res = await fetch(`${baseUrl}/api/auth/signin`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
