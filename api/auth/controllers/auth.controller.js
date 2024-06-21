ed59fd22f2a0797b2dd4c9c309ad8febabdb6b80
Mone19 <fuocoangelos@yahoo.de>
2024-06-21 13:06:59 +0200

    fix: store token in header

--- api/auth/controllers/auth.controller.js
+++ api/auth/controllers/auth.controller.js
@@ -66,9 +66,7 @@ export const signin = async (req, res, next) => {
     );
 
     const {password: userPassword, pass, ...rest} = validUser._doc;
-
-      res.status(200).cookie('access_token', token, {
-        httpOnly: true}).json(rest);
+    res.status(200).json({ token, rest});
 } catch (err) {
     next(err);
   }
@@ -81,8 +79,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin},process.env.JWT_SECRET);
       const {password, ...rest} = user._doc;
-      res.status(200).cookie('access_token', token, {
-        httpOnly: true}).json(rest);
+      res.status(200).json({ token, rest});
       } else {
         const generatedPassword = Math.random().toString(36).slice(-8) + 
         Math.random().toString(36).slice(-8);
@@ -96,11 +93,7 @@ export const google = async (req, res, next) => {
         await newUser.save();
         const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin},process.env.JWT_SECRET);
         const {password, ...rest} = newUser._doc;
-        res.status(200)
-        .cookie('access_token', token, {
-          httpOnly: true,
-        })
-        .json(rest);
+        res.status(200).json({ token, rest});
     }
   } catch (err) {
     next(err);
