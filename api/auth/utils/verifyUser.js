a2ee27f3fb79f72436b4135918fc7f3adc69cdcb
Mone19 <fuocoangelos@yahoo.de>
2024-06-23 11:11:17 +0200

    test: save token

--- api/auth/utils/verifyUser.js
+++ api/auth/utils/verifyUser.js
@@ -1,8 +1,15 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 export const verifyToken = (req, res, next) => {
+  //Neu
+  let token;
+  //Neu
   if ( req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    const token = req.headers.authorization.split(" ")[1];
+    // const token = req.headers.authorization.split(" ")[1];
+    //Neu
+    token = req.headers.authorization.split(" ")[1];
+    //Neu
+
   }
   if (!token) {
     return next(errorHandler(401, "Unauthorized"));
