dc019344ea94eb014c4c787a2aebfef4d6c2ab8b
onben <ons.benyoussef12@outlook.com>
2024-07-01 09:44:36 +0200

    test: update cypress test

--- e2e-testing/cypress/e2e/aiq-blog.cy.js
+++ e2e-testing/cypress/e2e/aiq-blog.cy.js
@@ -7,6 +7,7 @@ describe('visit  AIQ-Blog Homepage', () => {
     // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('http://localhost:5173/'),
     cy.get('.font-medium > :nth-child(5) > .py-1', {timeout:6000}).click({force: true , multiple: true})
+    cy.wait(3000)
     cy.slowDownEnd();
   })
 
