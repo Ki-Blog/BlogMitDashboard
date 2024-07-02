6d29499c725373e5b0432fc6a35b2f5bc8de2dd0
onben <ons.benyoussef12@outlook.com>
2024-07-02 11:59:49 +0200

    test: update test-files

--- e2e-testing/cypress/e2e/aiq-blog.cy.js
+++ e2e-testing/cypress/e2e/aiq-blog.cy.js
@@ -5,7 +5,6 @@ describe('visit  AIQ-Blog Homepage', () => {
   it('shows posts', () => {
     cy.slowDown(100000);
     cy.visit('https://www.aiq-blog.de/')
-    // cy.visit('http://localhost:5173/'),
     cy.get('.font-medium > :nth-child(5) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.wait(3000)
     cy.slowDownEnd();
@@ -14,7 +13,6 @@ describe('visit  AIQ-Blog Homepage', () => {
   it('shows ueber uns', () => {
     cy.slowDown(100000);
     cy.visit('https://www.aiq-blog.de/')
-    // cy.visit('http://localhost:5173/'),
     cy.get('.font-medium > :nth-child(3) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
   })
@@ -22,7 +20,6 @@ describe('visit  AIQ-Blog Homepage', () => {
   it('shows start Seite', () => {
     cy.slowDown(100000);
     cy.visit('https://www.aiq-blog.de/')
-    // cy.visit('http://localhost:5173/'),
     cy.get('.font-medium > :nth-child(1) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
   })
@@ -30,7 +27,6 @@ describe('visit  AIQ-Blog Homepage', () => {
   it('shows start Seite with Logo', () => {
     cy.slowDown(100000);
     cy.visit('https://www.aiq-blog.de/')
-    // cy.visit('http://localhost:5173/'),
     cy.get('.space-x-3 > .flex > .h-11').click()
     cy.slowDownEnd();
   })
@@ -41,7 +37,6 @@ describe('visit  AIQ-Blog Homepage', () => {
   
   it('shows posts', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(5) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.wait(3000)
@@ -50,7 +45,6 @@ describe('visit  AIQ-Blog Homepage', () => {
 
   it('shows ueber uns', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(3) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
@@ -58,7 +52,6 @@ describe('visit  AIQ-Blog Homepage', () => {
 
   it('shows start Seite', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(1) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
@@ -66,7 +59,6 @@ describe('visit  AIQ-Blog Homepage', () => {
 
   it('shows start Seite with Logo', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.space-x-3 > .flex > .h-11').click()
     cy.slowDownEnd();
@@ -79,18 +71,17 @@ require('cypress-slow-down/commands');
 describe('test Admin user in  AIQ-Blog ', () => {
     it('shows Admin functions', () => {
       cy.slowDown(100000);
-      // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/'),
       cy.visit('https://www.aiq-blog.de/'),
       cy.get('a > .group > .items-stretch').click(),
       cy.get('#email-desktop').type('ons123@hotmail.com'),
       cy.get('#password-desktop').type('123456789'),
       cy.get('.mt-12 > .flex-col > .from-purple-600 > .items-stretch').click()
-    //   cy.wait(20000)
       cy.get('[data-testid="flowbite-avatar-img"]', { timeout: 10000 }).should('be.visible').click(),
       cy.get('a > li > .flex').click(),
       cy.get('[href="/dashboard?tab=dash"] > .flex').click(),
       cy.get('[data-testid="flowbite-avatar-img"]').click(),
       cy.get('a > li > .flex').click(),
       cy.get('[href="/dashboard?tab=users"] > .flex').click()
+      cy.get('[href="/dashboard?tab=comments"] > .flex').click() 
     })
   })
--- e2e-testing/cypress/e2e/aiq-blogmitdomain.cy.js
+++ e2e-testing/cypress/e2e/aiq-blogmitdomain.cy.js
@@ -4,16 +4,13 @@ describe('visit  AIQ-Blog Homepage', () => {
   
   it('shows posts', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(5) > .py-1', {timeout:6000}).click({force: true , multiple: true})
-    cy.wait(3000)
     cy.slowDownEnd();
   })
 
   it('shows ueber uns', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(3) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
@@ -21,7 +18,6 @@ describe('visit  AIQ-Blog Homepage', () => {
 
   it('shows start Seite', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.font-medium > :nth-child(1) > .py-1', {timeout:6000}).click({force: true , multiple: true})
     cy.slowDownEnd();
@@ -29,7 +25,6 @@ describe('visit  AIQ-Blog Homepage', () => {
 
   it('shows start Seite with Logo', () => {
     cy.slowDown(100000);
-    // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/')
     cy.visit('https://www.aiq-blog.de/'),
     cy.get('.space-x-3 > .flex > .h-11').click()
     cy.slowDownEnd();
@@ -42,49 +37,39 @@ require('cypress-slow-down/commands');
 describe('test Admin user in  AIQ-Blog ', () => {
     it('shows Admin functions', () => {
       cy.slowDown(100000);
-      // cy.visit('http://aiq-frontend.s3-website.eu-central-1.amazonaws.com/'),
       cy.visit('https://www.aiq-blog.de/'),
       cy.get('a > .group > .items-stretch').click(),
       cy.get('#email-desktop').type('ons123@hotmail.com'),
       cy.get('#password-desktop').type('123456789'),
       cy.get('.mt-12 > .flex-col > .from-purple-600 > .items-stretch').click()
-    //   cy.wait(20000)
       cy.get('[data-testid="flowbite-avatar-img"]', { timeout: 10000 }).should('be.visible').click(),
       cy.get('a > li > .flex').click(),
       cy.get('[href="/dashboard?tab=dash"] > .flex').click(),
       cy.get('[data-testid="flowbite-avatar-img"]').click(),
       cy.get('a > li > .flex').click(),
       cy.get('[href="/dashboard?tab=users"] > .flex').click()
-      // cy.get('.overflow-x-auto > .text-lg').click()
-    //   cy.visit('https://www.aiq-blog.de/dashboard?tab=comments'),
       cy.get('[href="/dashboard?tab=comments"] > .flex').click() 
+      cy.wait(1000)
       cy.get('[href="/dashboard?tab=profile"] > .flex').click()
       cy.get('a > .group > .items-stretch').click(),
       cy.get('#title').type('new post for test'),
-    //   cy.wait(1000);
       cy.get('.flex-col > .lg\\:inline', { timeout: 10000 }).should('be.visible').select('Canva')
       cy.get('.ql-editor').type('my new Post')
       cy.get('[type="submit"] > .items-stretch').click()
       cy.get('[data-testid="flowbite-avatar-img"]').click(),
-      // cy.wait(10000); 
       cy.get('a > li > .flex').click()
-      // cy.wait(10000); 
-     cy.visit('https://www.aiq-blog.de/dashboard?tab=posts')
+      cy.visit('https://www.aiq-blog.de/dashboard?tab=posts')
       cy.get('[href="/dashboard?tab=posts"] > .flex', { timeout: 20000 }).should('be.visible').click()
-      // cy.wait(5000)
       cy.get('.bg-\\[\\#b8bfd71e\\] > :nth-child(6)', { timeout: 10000 }).should('be.visible').click()
       cy.get('#title').clear().type('updated post for test')
       cy.get('[type="submit"] > .items-stretch').click()
       cy.get('[data-testid="flowbite-avatar-img"]').click(),
       cy.get('a > li > .flex').click()
-      cy.get('[href="/dashboard?tab=posts"] > .flex', { timeout: 10000 }).click()
-    //   cy.get('[href="/dashboard?tab=posts"] > .flex', { timeout: 10000 }).should('exist').click()
-    //   cy.get('[href="/dashboard?tab=posts"] > .flex').click()
+      cy.get('[href="/dashboard?tab=posts"] > .flex', { timeout: 10000 }).should('exist').click()
       cy.get('.bg-\\[\\#b8bfd71e\\] > :nth-child(5)', { timeout: 10000 }).should('be.visible').click()
       cy.get('.gap-4 > .border-transparent > .flex').click()
       cy.slowDownEnd();
     
     })
-  
   
   })
